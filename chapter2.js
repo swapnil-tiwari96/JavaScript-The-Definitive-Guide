f558246cca96b0acf67c5e165b755d09e3113fae
Swapnil Tiwari <swaptiwari@outlook.com>
2023-12-31 02:47:20 +0530

    line deletion

--- chapter2.js
+++ chapter2.js
@@ -10,13 +10,8 @@ multiple line
 comment */
 
 // A literal is a data value that appears directly in a program
-
 // identifiers are used to name constants, variables, properties, functions, and classes and to provide labels for certain loops in JavaScript code.
-
 // JS reserves certain identifiers for use by the language itself.These “reserved words” cannot be used as regular identifiers.
-
 // JavaScript programs are written using the Unicode character set, and you can use any Unicode characters in strings and comments.
-
 // Some computer hardware and software cannot display, input, or correctly process the full set of Unicode characters. To support programmers and systems using older technology, JavaScript defines escape sequences that allow us to write Unicode characters using only ASCII characters.
-
 // Optional Semicolon - Don't think about it - its better to use semicolon when needed. 
